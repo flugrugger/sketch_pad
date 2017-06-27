7b4bd2d97ce94eed7c4d00b502a642b019460978
Ramtin Hakimjavadi <ramtin98@hotmail.com>
2017-06-26 21:49:05 -0400

    Added script.js, added jQuery, added container div

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	/*To Be Looped*/
+	$('.container').append('<div class="box"></div>');
+
+	$('.box').css({'height': '62.5px', 'width': '62.5px'})
+	/*To Be Looped*/
+});
