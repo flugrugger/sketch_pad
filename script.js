7fac7d1d8a564e071532b7b0bb0587ddd0df242e
Ramtin Hakimjavadi <ramtin98@hotmail.com>
2017-06-28 15:09:48 -0400

    Added clear/reset functionality to the buttons; user can now change the amount of squares in the grid

--- script.js
+++ script.js
@@ -10,12 +10,36 @@ $(document).ready(function() {
 	});
 });
 
+var newGrid = 16
+
+var newBoxLength = 25 
+
 function clearButton() {
 	$('.box').removeClass('black');
 }
 
-function resetButton() {
-	$('.box').removeClass('black');
 
-	var newGrid = prompt("How many squares would you like on each side of the sketch pad?");
+function changeBox() {
+	$('.box').width(newBoxLength);
+		
+	$('.box').height(newBoxLength);
+}
+
+function resetButton() {
+	$('div').remove('.box');
+	
+	newGrid = prompt("How many squares would you like on each side of the sketch pad?");
+
+	newBoxLength = 400/newGrid;
+
+	for (var i=0; i<(newGrid*newGrid); i++) {
+		var i = $('.box').length;
+		$('.container').append('<div class="box"></div>');
+	}
+	
+	$('.box').on('mouseenter', function() {
+		$(this).addClass('black');
+	});
+
+	changeBox();
 }
